dcd888040a931cc17ee71b223c0b089f61c4cb45
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-08-24 23:42:25 +0200

    seeds for deeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.record_timestamps = false
+begin
+  Deed.find_or_create_by({
+    person: 'David',
+    action: "I shared an umbrella with some stranger during today's heavy rain so she can go back home",
+    created_at: DateTime.new(2014, 7, 21, 16, 4),
+    updated_at: DateTime.new(2014, 7, 21, 16, 4)
+  })
+  Deed.find_or_create_by({
+    person: 'Tom',
+    action: "I brought some brownie to the office and shared it with my colleagues",
+    created_at: DateTime.new(2014, 7, 21, 15, 57),
+    updated_at: DateTime.new(2014, 7, 21, 15, 57)
+  })
+  Deed.find_or_create_by({
+    person: 'Marta',
+    action: "I'd made coffee to my husband before he woke up this morning.:)",
+    created_at: DateTime.new(2014, 7, 21, 1, 1),
+    updated_at: DateTime.new(2014, 7, 21, 1, 1)
+  })
+  Deed.find_or_create_by({
+    person: 'Kate',
+    action: 'I helped a blind person to go through a road',
+    created_at: DateTime.new(2014, 7, 20, 13, 1),
+    updated_at: DateTime.new(2014, 7, 20, 13, 1)
+  })
+ensure
+  ActiveRecord::Base.record_timestamps = true
+end
