88fcfc91af95194e4271219439a6774f4cd961a0
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-09-01 20:27:10 +0200

    rules draft

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'landing#show'
 
   resources :deeds, only: [:create]
+
+  get 'rules' => 'pages#rules'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
