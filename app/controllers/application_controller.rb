c9ea72626ead5716b43bcc1fcb637ce051d6b780
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-10-16 18:43:27 +0200

    ng-init with current_user_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,12 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_user_name
+    current_user.try(:name)
+  end
+
   helper_method :current_user
+  helper_method :current_user_name
 
   include ActionController::Serialization
 
