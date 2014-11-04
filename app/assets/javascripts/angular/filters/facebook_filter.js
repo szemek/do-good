a68b9895cf3d737491ad5fca31094ea311f8d924
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-11-04 14:44:44 +0100

    improvements to facebook filter & like button

--- app/assets/javascripts/angular/filters/facebook_filter.js
+++ app/assets/javascripts/angular/filters/facebook_filter.js
@@ -1,8 +1,20 @@
 app.filter('facebook_url', ['$sce', function($sce){
   return function(input){
-    var url = 'http://www.facebook.com/plugins/like.php?href=';
-    url += encodeURIComponent(input);
-    url += '&width&layout=button_count&action=like&show_faces=false&share=false&height=21&appId=512390572238099';
+    var url = 'http://www.facebook.com/plugins/like.php?';
+
+    var options = $.param({
+      href: input,
+      locale: 'en_US',
+      layout: 'button_count',
+      action: 'like',
+      show_faces: 'false',
+      share: 'false',
+      height: '21',
+      appId: '512390572238099'
+    });
+
+    url += options;
+
     return $sce.trustAsResourceUrl(url);
   };
 }]);
