e18ad1b6a85a4c48f9aaeab7b6268056b43a9be1
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-11-05 16:00:54 +0100

    disable double scrolling
    http://stackoverflow.com/questions/3656592/how-to-programmatically-disable-page-scrolling-with-jquery

--- app/assets/javascripts/angular/controllers/rules_controller.js
+++ app/assets/javascripts/angular/controllers/rules_controller.js
@@ -3,9 +3,15 @@ app.controller('RulesController', ['$scope', function($scope){
 
   $scope.displayRules = function(){
     $scope.showRules = true;
+    $('body').css({
+      'overflow': 'hidden'
+    });
   }
 
   $scope.hideRules = function(){
     $scope.showRules = false;
+    $('body').css({
+      'overflow': 'auto'
+    });
   }
 }]);
