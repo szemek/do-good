38d412ac67da409ddbfc413cc6f45265e34b3b32
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-05-14 18:59:00 +0200

    removed sections by date

--- app/assets/javascripts/angular/services/deeds_service.js
+++ app/assets/javascripts/angular/services/deeds_service.js
@@ -1,36 +1,10 @@
 app.factory('DeedsService', ['$resource', function($resource) {
-  var beginningOfDay = function(timestamp) {
-    var date = new Date(timestamp);
-    date.setHours(0);
-    date.setMinutes(0);
-    date.setSeconds(0);
-    return date.valueOf();
-  };
-
-  var sortDescByTimestamp = function(collection) {
-    return _.sortBy(collection, function(item) { return -item.timestamp; });
-  };
-
-  var groupByBeginningOfDay = function(collection) {
-    return _.groupBy(collection, function(item) {
-      return beginningOfDay(item.timestamp);
-    });
-  };
-
   return {
     fetch: function($scope, params, callback) {
       var Deed = $resource('api/deeds.json');
       params = params || {page: 1};
       Deed.get(params).$promise.then(function(data) {
-        var collection = groupByBeginningOfDay(data.deeds);
-        collection = _.map(collection, function(values, key) {
-          return {
-            timestamp: key,
-            deeds: sortDescByTimestamp(values)
-          };
-        });
-
-        $scope.collection = sortDescByTimestamp(collection);
+        $scope.deeds = data.deeds;
 
         if (callback !== undefined) {
           _.defer(callback);
